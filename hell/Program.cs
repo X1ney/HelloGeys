c1c0a9e07c3777a098734e450a68a7a8949f8dd6
X1ney <andreyka150104@gmail.com>
2023-04-26 17:39:25 +0700

    CreateProgram
    main program

---
+++ hell/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            int A, B;
+            Console.WriteLine("Введите А: ");
+            A = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите B: ");
+            B = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
